c69aa22a8651bf6ed012ae7e144605e2c38cc87b
Hermotimos <lukas.kozicki@gmail.com>
2019-04-29 21:20:23 +0200

    new bug fixed

--- browsing_flow.py
+++ browsing_flow.py
@@ -108,7 +108,6 @@ def start_browsing():
     If current page is records page, do nothing (let do_browsing() overtake).
     """
     report.report_function(set_strony)
-    report.report_function(click_back_n_times())
     start_point = report.report_function(determine_startpoint)
     if start_point == 1:
         report.report_function(click_status_and_search)
